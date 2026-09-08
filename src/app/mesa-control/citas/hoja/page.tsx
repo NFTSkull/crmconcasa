df6e06ccf703426aaecfb2db14118720aa51e29b
NFTSkull <111264923+NFTSkull@users.noreply.github.com>
2026-09-08 11:27:05 -0600

    feat(agenda): ruta hoja operativa CRM

---
+++ src/app/mesa-control/citas/hoja/page.tsx
@@ -0,0 +1,5 @@
+import { MesaAgendaHojaOperativaClient } from "@/components/mesa-control/MesaAgendaHojaOperativaClient";
+
+export default function MesaAgendaHojaOperativaPage() {
+  return <MesaAgendaHojaOperativaClient />;
+}
